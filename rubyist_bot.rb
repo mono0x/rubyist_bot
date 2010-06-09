e00a8f3ef233a2cf16fde4f96f848f8890e5ef4f
mono <mono@monoweb.info>
2010-06-10 00:40:19 +0900

    Added the similarity-based filter

--- rubyist_bot.rb
+++ rubyist_bot.rb
@@ -42,6 +42,41 @@ class Tracker
 
 end
 
+def levenshtein_distance(lhs, rhs)
+  width = lhs.size + 1
+  height = rhs.size + 1
+  d = Array.new(width * height, 0)
+
+  width.times do |i|
+    d[i] = i
+  end
+  height.times do |i|
+    d[i * width] = i
+  end
+
+  1.upto(height - 1) do |y|
+    1.upto(width - 1) do |x|
+      cost = lhs[x - 1] == rhs[y - 1] ? 0 : 1
+      i = x + y * width
+      d[x + y * width] = [
+        d[i - 1] + 1,
+        d[i - width] + 1,
+        d[i - (width + 1)] + cost,
+      ].min
+    end
+  end
+  d.last
+end
+
+def remove_uri(src)
+  src.gsub URI.regexp(['http', 'https']), ''
+end
+
+def similarity(lhs, rhs)
+  max = [lhs.size, rhs.size].max
+  max > 0 ? 1.0 - levenshtein_distance(lhs, rhs).to_f / max : 1.0
+end
+
 CONFIG = JSON.parse(open('config.json', 'r:utf-8').read)
 
 ACCOUNT = CONFIG['account']
@@ -50,6 +85,8 @@ CONSUMER_TOKEN = CONFIG['consumer_token']
 CONSUMER_SECRET = CONFIG['consumer_secret']
 ACCESS_TOKEN = CONFIG['access_token']
 ACCESS_SECRET = CONFIG['access_secret']
+BLOCK_SIMILARITY_SAMPLES = CONFIG['block']['similarity']['samples']
+BLOCK_SIMILARITY_THRESHOLD = CONFIG['block']['similarity']['threshold']
 BLOCK_WORDS = CONFIG['block']['word']
 BLOCK_NAMES = CONFIG['block']['screen_name']
 
@@ -58,6 +95,8 @@ KEYWORD = 'ruby'
 oauth = Twitter::OAuth.new(CONSUMER_TOKEN, CONSUMER_SECRET)
 oauth.authorize_from_access ACCESS_TOKEN, ACCESS_SECRET
 twitter = Twitter::Base.new(oauth)
+
+samples = []
 
 begin
   Tracker.start(ACCOUNT, PASSWORD, KEYWORD) do |status|
@@ -68,7 +107,13 @@ begin
     screen_name = status['user']['screen_name']
     next if screen_name == ACCOUNT
     next if BLOCK_NAMES.any?{|n| screen_name[n]}
-    text = text.gsub(/([\@\#])([[:alnum:]_]+)/) {"#{$1}{#{$2}}"}
+    text_without_uri = remove_uri(text).tr('A-Z', 'a-z').gsub(KEYWORD, '')
+    next if samples.any? {|t|
+      similarity(t, text_without_uri) > BLOCK_SIMILARITY_THRESHOLD
+    }
+    samples.shift if samples.size >= BLOCK_SIMILARITY_SAMPLES 
+    samples.push text_without_uri
+    text = text.gsub(/([\@\#])(\w+)/) {"#{$1}{#{$2}}"}
     text = text.gsub(/(?<=^|[^\w])(#{KEYWORD})(?=$|[^\w])/i) {
       $1.tr("A-Za-z", "Ａ-Ｚａ-ｚ")
     }
