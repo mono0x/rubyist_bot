86e81d64d60f2b7fb5a893f8526f5bd35ac98c0e
mono <mono@monoweb.info>
2011-07-23 14:12:36 +0900

    Added the reloading mechanism on SIGHUP

--- rubyist_bot.rb
+++ rubyist_bot.rb
@@ -20,47 +20,64 @@ require 'retweetfilter'
 require 'similarityfilter'
 require 'wordfilter'
 
-config = ConfigLoader.load
+class RubyistBot
 
-bayesian = Bayesian.new(:path => 'data/bayesian.dat')
+  def initialize
+    reload_config
+  end
 
-consumer = OAuth::Consumer.new(
-  config.consumer_token,
-  config.consumer_secret,
-  :site => 'https://api.twitter.com')
+  def reload_config
+    @config = ConfigLoader.load
+    @bayesian = Bayesian.new(:path => 'data/bayesian.dat')
 
-access_token = OAuth::AccessToken.new(
-  consumer,
-  config.access_token,
-  config.access_secret)
+    @consumer = OAuth::Consumer.new(
+      @config.consumer_token,
+      @config.consumer_secret,
+      :site => 'https://api.twitter.com')
 
-stream_consumer = OAuth::Consumer.new(
-  config.consumer_token,
-  config.consumer_secret,
-  :site => 'http://stream.twitter.com')
+    @access_token = OAuth::AccessToken.new(
+      @consumer,
+      @config.access_token,
+      @config.access_secret)
 
-stream_access_token = OAuth::AccessToken.new(
-  stream_consumer,
-  config.access_token,
-  config.access_secret)
+    @stream_consumer = OAuth::Consumer.new(
+      @config.consumer_token,
+      @config.consumer_secret,
+      :site => 'http://stream.twitter.com')
 
-filters = [
-  HasTextFilter.new,
-  RetweetFilter.new,
-  JapaneseFilter.new,
-  HashtagFilter.new,
-  LengthFilter.new(:length => config.block.text_length),
-  WordFilter.new(:text => config.block.word, :screen_name => config.block.screen_name),
-  SimilarityFilter.new(:keywords => config.keywords, :sample_count => config.block.similarity.sample_count, :threshold => config.block.similarity.threshold),
-  StatusLogger.new,
-  BayesianFilter.new(:bayesian => bayesian),
-  BlockFilter.new(consumer, access_token),
-]
+    @stream_access_token = OAuth::AccessToken.new(
+      @stream_consumer,
+      @config.access_token,
+      @config.access_secret)
 
-rubytter = OAuthRubytter.new(access_token)
+    @filters = [
+      HasTextFilter.new,
+      RetweetFilter.new,
+      JapaneseFilter.new,
+      HashtagFilter.new,
+      LengthFilter.new(:length => @config.block.text_length),
+      WordFilter.new(:text => @config.block.word, :screen_name => @config.block.screen_name),
+      SimilarityFilter.new(:keywords => @config.keywords, :sample_count => @config.block.similarity.sample_count, :threshold => @config.block.similarity.threshold),
+      StatusLogger.new,
+      BayesianFilter.new(:bayesian => @bayesian),
+      BlockFilter.new(@consumer, @access_token),
+    ]
 
-Stream.new(stream_consumer, stream_access_token).filter(config.keywords) do |status|
-  next unless filters.all? {|f| f.match status }
-  rubytter.retweet status.id
+    @rubytter = OAuthRubytter.new(@access_token)
+  end
+
+  def run
+    Signal.trap :HUP do
+      reload_config
+    end
+
+    Stream.new(@stream_consumer, @stream_access_token).filter(@config.keywords) do |status|
+      next unless @filters.all? {|f| f.match status }
+      @rubytter.retweet status.id
+    end
+  end
+
 end
+
+RubyistBot.new.run
 
